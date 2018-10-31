d34e06e4d76294968bd92e1f65c1e20fad89eb7d
dx <327087190@qq.com>
2018-10-31 11:02:23 +0800

    updata：新建store文件夹

--- template/src/main.js
+++ template/src/main.js
@@ -10,7 +10,7 @@ import router from './router'
 
 {{#vuex}}  //vuex为true的时候就会写入这些
 import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import store from  './store/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import store from  './store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
 
